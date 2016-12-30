615add5f5cf673cf784ee3d6b28c098dcf5790ee
RWaltersMA <robert.walters@10gen.com>
2016-12-30 11:29:45 -0500

    upload, view and delete photo functionally compelete

--- app.js
+++ app.js
@@ -5,19 +5,13 @@ var ObjectID=require('mongodb').ObjectID;
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var multer=require('multer');
-//var fs = require('fs');
 var url = require('url');
+var settings=require('./config.js');  //change monogodb server location here
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 var db;
- 
-var settings = {
-    host: '192.168.0.16',
-    port: 27017,
-    database: 'MyDocumentsDB'
-};
  
 var connectionString = url.format({
     protocol: 'mongodb',
@@ -26,7 +20,8 @@ var connectionString = url.format({
     port: settings.port,
     pathname: settings.database
 });
-// The client will send any metadata over including the desired filename, etc.
+// This object is used when the user POSTs a new photo.
+// Note that the client will send any metadata over including the desired filename, etc.
 // the function below will fetch them from the incoming req object at run-time
 var storage = require('multer-gridfs-storage')({
     url: connectionString,
@@ -39,84 +34,38 @@ var storage = require('multer-gridfs-storage')({
       cb(null, JSON.parse(req.body['metadata']));
     }
         });
-    var upload = multer({ storage: storage });
 
+var upload = multer({ storage: storage });
 
-app.get('/photos',function (req,res)
-{
-    res.writeHead(200, {'Content-Type':'text/html'});
-  
-    MongoClient.connect(connectionString, function(err, db) 
-    {
-        assert.equal(null, err);
-
-    var bucket = new mongodb.GridFSBucket(db);/*, 
-        {
-        chunkSizeBytes: 131072,
-        bucketName: 'myfiles'
-        });*/
-        var c=bucket.find().count(function(err, count) {
-       
-        //Eventually wrap results up into a JSON to return to caller, for now just return HTML
-        res.write('<html><body><p>File count : ' + String(count) + '</p>');
-
-      });
-        var d=bucket.find().toArray().then((docs) => {
-       
-       docs.forEach((item, idx, array) => 
-       {
-           var size = parseInt(item['length'], 10);
-           size=size/1024; // Convert to kB
-             //Eventually wrap results up into a JSON to return to caller, for now just return HTML
-             res.write('<p><a href=\'/photos/' + item['filename'] + '\'>' + item['filename'] + '</a> (' + Number(size).toFixed(2) + ' kB)</p>');
-        });
-       
-       db.close();
-
-       res.write('</body></html>')
-       res.end();
-       
-   }).catch((err) => {
-       
-       res.write(err.stack);
-    });
-                        
-    });
-
-})
-
-//Usage /photos/FILENAME
+//Usage /photos/_id where _id is the ID saved in fs.files for the specific photo
+//This method will need work to address error handling, for example passing unknown ID will throw an exception
 app.get('/photos/:PhotoId', function (req, res) {
      
      try{
-         
-        var pfile = req.params.PhotoId;
         var bucket = new mongodb.GridFSBucket(db);
-        var photo_id=new ObjectID(pfile);
+        var photo_id=new ObjectID(req.params.PhotoId);
         var contentType="";
-        // db.collection(abs,,function (err,doc) { doc.findOne})
+        // we need to make sure we get the right contentType and pass it to the header
         var PhotoFile = db.collection("fs.files", function (err,result) {
+            if (err) { throw ('Could not enumerate files collection'); }
             result.findOne({"_id" : photo_id}, function (err,thephoto) {
-                   contentType=thephoto['contentType'];
-     }) }); 
-        
-        
-       /* .count(function (err,count) {
-            if (count==0)
-            {
-                res.setStatus(404);
-                return false;
-            }
-        });*/
-        //console.log(files); //['contentType']
-        res.writeHead(200, {'Content-Type': contentType});//'image/jpg'});
-        bucket.openDownloadStream(photo_id).pipe(res);
+                if (err) { throw('Could not find photo with specified _id'); }
+
+                    contentType=thephoto['contentType'];
+             }) }); 
+
+            res.writeHead(200, {'Content-Type': contentType});//  i.e. 'image/jpg'
+            bucket.openDownloadStream(photo_id).pipe(res);
+      
      }
      catch (e)
      {
-       throw('Not a valid photo ID');
+        res.writeHead(200, {'Content-Type':'text/html'});
+        res.write('<html><body><p>Error reading photo : ' + e);
+       res.end();
        return false;
      }
+
 })
 //Render home page 
 app.get('/', function(request, response) {
@@ -124,31 +73,52 @@ app.get('/', function(request, response) {
     var Photos=[];
 
     var bucket = new mongodb.GridFSBucket(db);
+    var TotalSize=0;
+    var NumberOfPhotos=0;
+
+    //consider paging for very large lists, here we are getting all file metadata into memory
     var d=bucket.find().toArray().then((docs) => {
     
        docs.forEach((item, idx, array) => 
        {
            var size = parseInt(item['length'], 10);
            size=size/1024; // Convert to kB
+           TotalSize+=size;
+           
+           var description="";
+           var metadata = item['metadata'];
+           //don't crash and burn if there are some metadata in there that we aren't expecting
+           //as-is we only expect a description key but as you add more/change you will need to address it here
+           try
+           {
+                if (metadata)
+                {
+                            if (metadata.hasOwnProperty('description')==true) description=metadata.description;
+                } 
 
+           }
+           catch (e)
+           {
+               description="";
+           }
            Photos.push(
                {
                    length: size,
                    filename: item['filename'],
-                   id: item['_id']
+                   id: item['_id'],
+                   description: description
                }
            );
-       }
-       );
+       });
 
         
-          response.render('index',{Photos:Photos});
-        }
-        );
+          response.render('index',{Photos:Photos, TotalPhotoSize: TotalSize});
+        });
 
      
         });
 
+//Render Upload photo page
 app.get('/new', function(request, response) {
 
     response.render('new');
@@ -159,12 +129,47 @@ app.get('/new', function(request, response) {
 app.post('/upload', upload.single('file'), function (req, res, next) 
 {
     
-    //console.log(req.body['metadata']);
-    
     res.sendStatus(201);
     res.end();
 
 })
+
+//This function will remove a photo from the database for a given _id
+app.get('/delete/:PhotoId', function (req, res) {
+     
+     try{
+        
+        var photo_id=new ObjectID(req.params.PhotoId);
+        var grid = new mongodb.GridFSBucket(db);
+        var PhotoFile = db.collection("fs.files", function (err,result) {
+            if (err)
+            {
+                throw('Could not enumerate files collection');
+                return false;
+            }
+            result.findOne({"_id" : photo_id}, function (err,thephoto) {
+                if (err)
+                {
+                    throw('Could not find the _id');
+                    return false;
+                }
+
+                grid.delete(photo_id,function (err) {
+                    if (err){
+                            throw('Could not delete the given _id');
+                            }  
+                                                    });
+            });
+        });
+     }
+     catch (e)
+     {
+         //Something went wrong, bad ID, etc so let's tell the user
+         console.log(e);
+     }
+     res.redirect('/');
+})
+
 //Main Entry Point, create the MongoDB Connection and use connection-pooling from the driver
 MongoClient.connect(connectionString, function(err, database) {
 
@@ -172,7 +177,6 @@ MongoClient.connect(connectionString, function(err, database) {
     if(err) throw err;
 
     db = database;
-   
 
   // Start the application after the database connection is ready
   app.listen(3000);
---
+++ config.js
@@ -0,0 +1,8 @@
+
+var settings = {
+    host: '192.168.0.16',
+    port: 27017,
+    database: 'MyDocumentsDB'
+};
+
+module.exports =settings;
